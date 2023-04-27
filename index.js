913af300cb57a221a0f6a2ee2bf5536e1728bff8
PA <ngophuonganh9a4.ptin@gmail.com>
2023-04-28 02:36:03 +0700

    update file index.js

--- index.js
+++ index.js
@@ -19,3 +19,35 @@ client = new MongoClient("mongodb://127.0.0.1:27017");
 client.connect();
 database = client.db("FashionData");
 fashionCollection = database.collection("Fashion");
+
+
+database = client.db("PaymentData"); //kết nối tới database
+paymentCollection = database.collection("Payment"); //truy suất collection
+paymentATMCollection = database.collection("PaymentATM")
+
+//Lệnh này truy vấn toàn bộ Fashion và trả về JsonArray.
+
+app.get("/payments",cors(),async (req,res)=>{
+    const result = await paymentCollection.find({}).toArray();
+    res.send(result)
+})
+
+app.get("/paymentATMs",cors(),async (req,res)=>{
+    const result = await paymentATMCollection.find({}).toArray();
+    res.send(result)
+})
+
+app.post("/payments",cors(),async(req,res)=>{
+    //put json Fashion into database
+    await paymentCollection.insertOne(req.body)
+    //send message to client(send all database to client)
+    res.send(req.body)
+})
+
+app.post("/paymentATMs",cors(),async(req,res)=>{
+    //put json Fashion into database
+    await paymentATMCollection.insertOne(req.body)
+    //send message to client(send all database to client)
+    res.send(req.body)
+})
+
